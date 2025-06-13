5dd911802089368ad9e072b0c81cf1e914ca1ceb
rizenkai <starlatifah5@gmail.com>
2025-06-14 00:17:50 +0700

    add cloudinary

--- middleware/cloudinaryUpload.js
+++ middleware/cloudinaryUpload.js
@@ -7,6 +7,9 @@ const storage = new CloudinaryStorage({
   params: {
     folder: 'documents', // semua file masuk folder ini di Cloudinary
     resource_type: 'raw', // pastikan semua file (pdf, docx, dll) diupload sebagai raw agar bisa diakses publik
+    use_filename: true, // gunakan nama file asli
+    unique_filename: false, // jangan random, biarkan nama file asli
+    overwrite: true, // boleh overwrite jika nama sama
   },
 });
 
